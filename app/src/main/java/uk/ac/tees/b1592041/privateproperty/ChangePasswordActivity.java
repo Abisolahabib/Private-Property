43cbecdca3f8793d849e99b3f1a79831af2fcaf2
Abisola <habibabisola@gmail.com>
2022-11-10 13:02:52 +0000

    created one time password and change password

---
+++ app/src/main/java/uk/ac/tees/b1592041/privateproperty/ChangePasswordActivity.java
@@ -0,0 +1,22 @@
+package uk.ac.tees.b1592041.privateproperty;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class ChangePasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_password);
+    }
+
+    public void changePwd(View view) {
+        Toast toast = Toast.makeText(this, R.string.toast_message,Toast.LENGTH_LONG);
+        toast.show();
+
+    }
+}
